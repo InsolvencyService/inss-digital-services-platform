732907b7d1a2185f39c8a2a45de0b246c9c2d441
Dave Cook <Dave.Cook@insolvency.gov.uk>
2025-10-02 15:40:51 +0100

    Created Test Data for User Management.

---
+++ Apps/UserManagement/Database/INSS.Platform.UserManagement.Database/Scripts/PostDeployment/TeardownTestData.sql
@@ -0,0 +1,22 @@
+﻿
+/*
+	Removes all test data from the database.
+*/
+
+DELETE FROM [dbo].[OrganisationUserApplicationRole]
+
+DELETE FROM [dbo].[OrganisationUser]
+
+DELETE FROM [dbo].[Organisation]
+
+DELETE FROM [dbo].[ApplicationRole]
+
+DELETE FROM [dbo].[Application]
+
+DELETE FROM [dbo].[Role]
+
+DELETE FROM [dbo].[User]
+
+DELETE FROM [dbo].[UserIdentity]
+
+DELETE FROM [dbo].[IdentityProvider]
