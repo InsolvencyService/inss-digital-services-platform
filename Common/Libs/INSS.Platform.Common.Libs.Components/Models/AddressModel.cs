8b062ea166084063dd228ae29b054f344c8c7f6b
Glen Stone <Glen.Stone@insolvency.gov.uk>
2025-10-17 09:35:56 +0100

    Removed unecessary files.

---
+++ Common/Libs/INSS.Platform.Common.Libs.Components/Models/AddressModel.cs
@@ -0,0 +1,8 @@
+﻿namespace INSS.Platform.Common.Libs.Components.Models
+{
+    public class AddressModel
+    {
+        public string Line1 { get; set; } = string.Empty;
+        public string Line2 { get; set; } = string.Empty;
+    }
+}
