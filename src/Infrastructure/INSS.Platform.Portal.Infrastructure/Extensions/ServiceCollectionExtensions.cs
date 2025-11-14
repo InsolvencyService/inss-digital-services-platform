46c49602074d6e49533e3aad3437216a501c58cd
Glen Stone <Glen.Stone@insolvency.gov.uk>
2025-11-14 10:21:12 +0000

    Moved POC projects into src folder following clean architecture.

---
+++ src/Infrastructure/INSS.Platform.Portal.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using INSS.Platform.Portal.Application.Resolvers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace INSS.Platform.Portal.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IUserSessionResolver, TestUserSessionResolver>();
+        return services;
+    }
+}
