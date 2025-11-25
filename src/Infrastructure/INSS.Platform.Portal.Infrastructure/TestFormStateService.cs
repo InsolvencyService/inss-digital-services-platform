90ad72f535278499eee7f44c5b002ce6b0dc887c
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2025-11-25 14:45:16 +0000

    Fix System.Text.RegularExpressions v4.3.0 vulnerability with change to use Microsoft.AspNetCore.Mvc.Abstractions instead of Microsoft.AspNetCore.Mvc and Microsoft.AspNetCore.Mvc.Core v2.3.0

--- src/Infrastructure/INSS.Platform.Portal.Infrastructure/TestFormStateService.cs
+++ src/Infrastructure/INSS.Platform.Portal.Infrastructure/TestFormStateService.cs
@@ -1,23 +1,23 @@
-﻿using INSS.Platform.Portal.Application.Services;
+﻿using System.Collections.Concurrent;
+using INSS.Platform.Portal.Application.Services;
 using INSS.Platform.Portal.Domain;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace INSS.Platform.Portal.Infrastructure;
 
-public sealed class TestFormStateService : IFormStateService, IDisposable
+public sealed class TestFormStateService : IFormStateService
 {
-    private readonly MemoryCache _cache = new(new MemoryCacheOptions());
+    private static readonly ConcurrentDictionary<string, FormModel> _cache = new();
 
     public Task<bool> FormExistsAsync(string sessionId)
     {
-        return Task.FromResult(this._cache.TryGetValue(sessionId, out _));
+        return Task.FromResult(_cache.ContainsKey(sessionId));
     }
     
     public Task<FormModel> GetAsync(string sessionId)
     {
         if (_cache.TryGetValue(sessionId, out var model))
         {
-            return Task.FromResult((FormModel)model!);
+            return Task.FromResult(model);
         }
 
         throw new InvalidOperationException($"Unable to find the form model for the session {sessionId}");
@@ -25,12 +25,7 @@ public sealed class TestFormStateService : IFormStateService, IDisposable
 
     public Task SaveAsync(string sessionId, FormModel model)
     {
-        _cache.Set(sessionId, model);
+        _cache[sessionId] = model;
         return Task.CompletedTask;
-    }
-
-    public void Dispose()
-    {
-        _cache.Dispose();
     }
 }
