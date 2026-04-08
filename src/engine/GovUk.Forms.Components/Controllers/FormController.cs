c4b66bf5a836507d5e1f55a985bc461564f509dd
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2026-04-08 09:00:38 +0100

    Add back button helper

--- src/engine/GovUk.Forms.Components/Controllers/FormController.cs
+++ src/engine/GovUk.Forms.Components/Controllers/FormController.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using GovUk.Forms.Application.Services;
 using GovUk.Forms.Components.Authentication;
+using GovUk.Forms.Components.Extensions;
 using GovUk.Forms.Domain;
 using GovUk.Forms.Domain.Primitives;
 using Microsoft.AspNetCore.Mvc;
@@ -22,7 +23,7 @@ public class FormController : Controller
     public async Task<IActionResult> Edit(string? state = null)
     {
         (ContentModel? Content, ContentPath? RedirectTo) result = await _formService.LoadAsync(new ContentPath(Request.Path), state);
-        ConfigureBackButton(result.Content);
+        ViewData.AddBackButton(result.Content);
         return result.RedirectTo is not null ? Redirect(result.RedirectTo) : View(result.Content);
     }
 
@@ -46,10 +47,5 @@ public class FormController : Controller
 
         ContentPath redirectTo = await _formService.SaveAsync(postedContent);
         return Redirect(redirectTo);
-    }
-
-    private void ConfigureBackButton(ContentModel? content)
-    {
-        ViewData["Back"] = content is PageModel page ? page.PreviousPagePath : null;
     }
 }
