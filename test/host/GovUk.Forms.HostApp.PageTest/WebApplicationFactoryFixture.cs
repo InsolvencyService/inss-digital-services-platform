beb3c275328bc0267245ca862653425642c162af
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2026-04-15 16:22:43 +0100

    Remove sample page test project and try alt filter to get pipeline working

--- test/host/GovUk.Forms.HostApp.PageTest/WebApplicationFactoryFixture.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace GovUk.Forms.HostApp.PageTest;
-
-[CollectionDefinition(Name)]
-public sealed class WebApplicationFactoryFixture : ICollectionFixture<TestWebApplicationFactory>
-{
-    public const string Name = "End-to-End";
-}
