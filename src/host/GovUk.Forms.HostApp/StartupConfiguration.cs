300405ed425721daef2e2f3e1293f1e84721527f
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2026-04-24 15:33:48 +0100

    Revert logging

--- src/host/GovUk.Forms.HostApp/StartupConfiguration.cs
+++ src/host/GovUk.Forms.HostApp/StartupConfiguration.cs
@@ -26,9 +26,6 @@ public class StartupConfiguration : IHostingStartup
             BrokerOptions brokerOptions = new();
             context.Configuration.GetSection("Broker").Bind(brokerOptions);
 
-            IServiceProvider sp = services.BuildServiceProvider();
-            ILogger logger = sp.GetRequiredService<ILogger>();
-            logger.LogInformation("The Identity provider is {ID}", brokerOptions.IdentityProvider?.ToString() ?? "NULL");
             services.AddSingleton<IAuthenticationProvider>(_ =>
             {
                 return brokerOptions.IdentityProvider switch
