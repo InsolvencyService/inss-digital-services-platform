a4825c92e9782aafee64033bead1adaa7c386637
Steve.Saunders <steve.saunders@insolvency.gov.uk>
2026-06-10 10:24:21 +0100

    Fix merge conflicts

--- src/apps/Demo.GovUk.Forms.ContactUs/StartupConfiguration.cs
+++ src/apps/Demo.GovUk.Forms.ContactUs/StartupConfiguration.cs
@@ -1,8 +1,7 @@
 using Demo.GovUk.Forms.ContactUs.Application.Factories;
 using Demo.GovUk.Forms.ContactUs.Builders;
-using Demo.GovUk.Forms.ContactUs.Factories;
+using GovUk.Forms.Application.Factories;
 using GovUk.Forms.Components;
-using GovUk.Forms.Components.Factories;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using StartupConfiguration = Demo.GovUk.Forms.ContactUs.StartupConfiguration;
