ab29ca3921fb55566cf769168a1ec45d83c9fca6
Glen.Stone <Glen.Stone@insolvency.gov.uk>
2026-07-31 11:41:02 +0100

    MEDS-1168 Retrieve and display Authorising Body data from config

---
+++ src/engine/GovUk.Forms.Domain/Search/AuthorisingBodyLookup.cs
@@ -0,0 +1,24 @@
+﻿namespace GovUk.Forms.Domain.Search;
+
+public sealed class AuthorisingBodyLookup
+{
+    public string AuthBodyCode { get; init; } = string.Empty;
+
+    public string AuthBodyName { get; init; } = string.Empty;
+
+    public string AuthBodyAddressLine1 { get; init; } = string.Empty;
+
+    public string AuthBodyAddressLine2 { get; init; } = string.Empty;
+
+    public string AuthBodyAddressLine3 { get; init; } = string.Empty;
+
+    public string AuthBodyAddressLine4 { get; init; } = string.Empty;
+
+    public string AuthBodyAddressLine5 { get; init; } = string.Empty;
+
+    public string AuthBodyPostcode { get; init; } = string.Empty;
+
+    public string Phone { get; init; } = string.Empty;
+
+    public string Website { get; init; } = string.Empty;
+}
